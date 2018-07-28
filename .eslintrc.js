541dbc772ce1b0919c13388120bfa62d1216353c
leosuoxxo <leo.suoxxo@gmail.com>
2018-07-28 18:13:24 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,31 @@
+/** @format */
+
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
+  extends: [
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
     '@vue/prettier'
   ],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: true
+      }
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
